c5362a10ff0f25902c1cfbfef4a86208df3d8223
Alex Kost <alezost@gmail.com>
2014-01-03 22:24:27 +0400

    Add "layout-" prefix to symbols from "layout.lisp"

--- layouts.lisp
+++ layouts.lisp
@@ -27,70 +27,70 @@
 (xlib::initialize-extensions *display*)
 (xlib:enable-xkeyboard *display*)
 
-(defun get-current-layout (display)
+(defun layout-get-current-layout (display)
   "Return current keyboard layout."
   (xlib:device-state-locked-group (xlib:get-state display)))
 
-(defun get-window-layout (win &optional (default nil))
+(defun layout-get-window-layout (win &optional (default nil))
   "Return keyboard layout of a specified window WIN.
 If a window does not have a layout property, return DEFAULT."
   (getf (xlib:window-plist (window-xwin win))
         :keyboard-layout default))
 
-(defun window-focus-changed (window previous-window)
-  (let ((current-layout (get-current-layout *display*)))
+(defun layout-window-changed (window previous-window)
+  (let ((current-layout (layout-get-current-layout *display*)))
     (when previous-window
       (setf (getf (xlib:window-plist (window-xwin previous-window))
                   :keyboard-layout)
             current-layout)
       (when window
-        (let ((window-layout (get-window-layout window current-layout)))
+        (let ((window-layout (layout-get-window-layout window current-layout)))
           (when (not (equal current-layout window-layout))
             (xlib:lock-group *display* :group window-layout)))))))
 
-(defun group-focus-changed (group previous-group)
-  (let ((previous-window (stumpwm::group-current-window previous-group))
-        (window (stumpwm::group-current-window group)))
-    (window-focus-changed window previous-window)))
+(defun layout-group-changed (group previous-group)
+  (let ((previous-window (group-current-window previous-group))
+        (window (group-current-window group)))
+    (layout-window-changed window previous-window)))
 
-(defcommand enable-per-window-layout () ()
+(defcommand layout-enable-per-window () ()
   "Enable changing keyboard layouts per window."
-  (add-hook *focus-window-hook* 'window-focus-changed)
-  (add-hook *focus-group-hook*  'group-focus-changed))
+  (add-hook *focus-window-hook* 'layout-window-changed)
+  (add-hook *focus-group-hook*  'layout-group-changed))
 
-(defcommand disable-per-window-layout () ()
-  "Enable changing keyboard layouts per window."
-  (remove-hook *focus-window-hook* 'window-focus-changed)
-  (remove-hook *focus-group-hook*  'group-focus-changed))
+(defcommand layout-disable-per-window () ()
+  "Disable changing keyboard layouts per window."
+  (remove-hook *focus-window-hook* 'layout-window-changed)
+  (remove-hook *focus-group-hook*  'layout-group-changed))
 
-(defvar *emacs-layout-key-alist*
+(defvar *layout-emacs-key-alist*
   '((0 . "s-7")
     (1 . "s-8")
     (2 . "s-9"))
   "Associations list of layout numbers and key bindings for emacs.
 Key bindings are strings accepted by `kbd'.")
 
-(defun get-emacs-layout-key (num)
+(defun layout-get-emacs-key (num)
   "Return a key value by a layout (group) number.
-Key binding is taken from `*emacs-layout-key-alist*'.
+Key binding is taken from `*layout-emacs-key-alist*'.
 Return nil, if there is no association."
-  (kbd (cdr (assoc num *emacs-layout-key-alist*))))
+  (kbd (cdr (assoc num *layout-emacs-key-alist*))))
 
-(defcommand set-layout (num) ((:number "layout number: "))
+(defcommand layout-set (num) ((:number "Layout number: "))
   "Set keyboard layout to a specified layout (xkb group) number.
 If current window is emacs, send an appropriate key to it.
-The key is defined by `get-emacs-layout-key'."
+The key is defined by `layout-get-emacs-key'."
   (if (utl-emacs-window-p)
-      (let ((key (get-emacs-layout-key num)))
+      (let ((key (layout-get-emacs-key num)))
         (when key
-          ;; (or (eq 0 (get-current-layout *display*))
+          ;; (or (eq 0 (layout-get-current-layout *display*))
           ;;     (xlib:lock-group *display* :group 0))
           (setq num 0)
           (utl-send-key key))))
   (xlib:lock-group *display* :group num)
   (xlib:display-finish-output *display*))
 
-(set-layout 0)
-(enable-per-window-layout)
+(layout-set 0)
+(layout-enable-per-window)
 
 ;;; layouts.lisp ends here
