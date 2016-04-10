551f41713638c0c04430f991f700b63f7b6c46ec
Alex Kost <alezost@gmail.com>
2016-04-10 08:15:03 +0300

    Get rid of a compilation warning

--- init.lisp
+++ init.lisp
@@ -15,6 +15,7 @@
 (defvar al/display-number
   (multiple-value-bind (_ array)
       (cl-ppcre:scan-to-strings ":([0-9]+)" (getenv "DISPLAY"))
+    (declare (ignore _))
     (if (vectorp array)
         (parse-integer (aref array 0))
         0))
