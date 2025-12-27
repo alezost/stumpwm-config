22d6bcc743c539f1b59dc352877e07be1b444304
Alex Kost <alezost@gmail.com>
2025-12-27 19:01:35 +0300

    init: Remove code to load "stumpwm-contrib" modules
    I have never used them anyway.

--- init.lisp
+++ init.lisp
@@ -64,20 +64,6 @@
         (load file)
         (format *error-output* "File '~a' doesn't exist." file))))
 
-(defun al/load-module (name)
-  "Load contributed stumpwm module NAME.
-Return nil, if the module does not exist.
-This function is similar to `load-module' command, except it returns nil
-instead of any error."
-  (let ((module (find-module (string-downcase name))))
-    (and module
-         (progn (asdf:operate 'asdf:load-op module)
-                t))))
-
-(set-module-dir
- (pathname-as-directory (concat (getenv "HOME")
-                                "/src/stumpwm-contrib")))
-
 (al/load "keys")
 (al/load "utils")
 (al/load "xkb")
